33fb9ec37313df6da5a1c86ee4fd87498a8c28d8
Kevin Sparks <sparksk16@gmail.com>
2015-05-26 13:49:43 -0400

    new test 4

--- newfile_functions.R
+++ newfile_functions.R
@@ -1,3 +1,4 @@
+# new test 4
 # OSM and ISM Generator: extract all individual similarity matrices (ISMs) 
 # and generate the overall similarity matrix(OSM) by summing up all ISMs
 OsmIsmGenerator <- function(path) {
